6502c5fb65652315669a7df73f4419ed7e5fbaae
Ryan Steen <ryan@ryansteen.com>
2018-11-30 17:47:46 -0800

    Port Change

--- src/config/http.js
+++ src/config/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const http = axios.create({
-  // baseURL: 'https://greentreehive.herokuapp.com/api'
-  baseURL: 'http://localhost:8080/api'
+  baseURL: 'https://greentreehive.herokuapp.com/api'
+  // baseURL: 'http://localhost:8080/api'
 })
